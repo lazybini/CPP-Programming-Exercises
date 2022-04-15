9dae4802e04d00e00f2d92030a094ca0747fd16d
lazybini <102787125+lazybini@users.noreply.github.com>
2022-04-15 10:30:49 +0900

    Add files via upload

--- Week6_Homework/Week6_Homework1/Complex V1/Complex.h
+++ Week6_Homework/Week6_Homework1/Complex V1/Complex.h
@@ -4,10 +4,10 @@ struct Complex {
 	double real;
 	double imag;
 };
-inline void setComplex(Complex& c, double r, double i) {
+inline void setComplex(Complex &c, double r, double i) {
 	c.real = r;
 	c.imag = i;
 };
-extern Complex readComplex(const char* msg = "ÂºÂ¹Â¼Ã’Â¼Ã¶ =");
-extern void printComplex(Complex c,const char* msg = "ÂºÂ¹Â¼Ã’Â¼Ã¶ =");
+extern Complex readComplex(const char* msg = "º¹¼Ò¼ö =");
+extern void printComplex(Complex c,const char* msg = "º¹¼Ò¼ö =");
 extern Complex addComplex(Complex a, Complex b);
